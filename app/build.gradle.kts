38f5eaa4154d5e863aed5a1822d65d88d8693eb0
Cheezestyck <carterdcrum@gmail.com>
2026-08-22 20:12:21 -0500

    Fix Deepgram transport dependency build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,4 +28,5 @@ android {
 dependencies {
     implementation("dev.rikka.shizuku:api:13.1.5")
     implementation("dev.rikka.shizuku:provider:13.1.5")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
