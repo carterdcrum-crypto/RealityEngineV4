8c13815b37d984d882a8ec390a32e052f62e6acf
Cheezestyck <carterdcrum@gmail.com>
2026-09-06 16:32:27 -0500

    fix(call): remove legacy call overlays from Pulse Deck

--- app/src/main/java/com/realityengine/v4/RealityEngineApp.kt
+++ app/src/main/java/com/realityengine/v4/RealityEngineApp.kt
@@ -13,9 +13,9 @@ class RealityEngineApp : Application() {
         super.onCreate()
         registerActivityLifecycleCallbacks(RealityOperatorSkin.callbacks)
         registerActivityLifecycleCallbacks(RealityVectorIconGuard.callbacks)
-        registerActivityLifecycleCallbacks(ConversationOSOverlay.callbacks)
+        registerActivityLifecycleCallbacks(ConversationOSNonCallCallbacks.callbacks)
         registerActivityLifecycleCallbacks(LiveTranscriptLayoutOverlay.callbacks)
-        registerActivityLifecycleCallbacks(SignalVisualOverlay.callbacks)
+        registerActivityLifecycleCallbacks(PulseDeckRuntime.callbacks)
         registerActivityLifecycleCallbacks(RecordingDiscoveryOverlay.callbacks)
         registerActivityLifecycleCallbacks(PostCallReviewHandoff.callbacks)
     }
